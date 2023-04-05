8a400ae9ac089de20ba505f99297491b122e5f62
Josué Valle <jvalle@bdgsa.net>
2023-04-05 11:37:02 -0600

    Se realizaron modificaciones

--- Backend_NodeJs_Express/middlewares/auth.js
+++ Backend_NodeJs_Express/middlewares/auth.js
@@ -3,17 +3,18 @@ require("dotenv").config();
 const secretkey = process.env.KEY;
 
 exports.isLoged = async (req, res, next) => {
-    if(req.headers.authorization){
-        try {
-            let token = req.headers.authorization.replace(/['",]+/g, '');
+    if(!req.headers.authorization){
+        return res.status(403).send({message: 'The request does not contain the authentication header'})
+    }else{
+        try{
+            let token = req.headers.authorization.replace(/['"]+/g, '');
             let payload = jwt.decode(token, secretkey);
             req.user = payload;
             next();
-        } catch (err) {
+        }catch(err){
             console.log(err);
-            return err;
+            return res.status(404).send({message: 'The token is not valid or has already expired'});
         }
-    }else{
-        return res.status(401).send({message: "The request does not contain the authentication header."});
+        
     }
 }
